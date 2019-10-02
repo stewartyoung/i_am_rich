e00ce2e054eb3ba91b095c8be963fd0c890d531f
Stewart Young <stewartyoung97@gmail.com>
2019-10-02 08:51:58 +0100

    added background colors and appbar title

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Text('Hello World')));
+// The main function is the starting point for all flutter apps
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[50],
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+      ),
+    ),
+  );
+}
